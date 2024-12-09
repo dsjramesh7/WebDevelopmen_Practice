8e7d0e5d036a2cf1e4fd10ee4c4f60fad3001189
dsjramesh7 <rameshdussa07@gmail.com>
2024-12-10 00:07:02 +0530

    practice

--- 2_JavaScript/projects/TodoApp_Custom/index.js
+++ 2_JavaScript/projects/TodoApp_Custom/index.js
@@ -1,6 +1,7 @@
 const inputEl = document.querySelector("#input");
 const taskListEl = document.querySelector("#todoList");
 
+//add feature
 const addTodo = () => {
   const inputvalue = inputEl.value.trim(); //for no space at beginning and end pe
 
@@ -12,16 +13,20 @@ const addTodo = () => {
   const newTaskDiv = document.createElement("div");
   const taskTextSpan = document.createElement("span");
   const deleteButton = document.createElement("button");
+  const editButton = document.createElement("button");
 
   taskTextSpan.textContent = inputvalue;
   deleteButton.textContent = "Delete Task";
+  editButton.textContent = "Edit Task";
 
   // for styling thing
   newTaskDiv.classList.add("task-item");
   taskTextSpan.classList.add("task-text");
   deleteButton.classList.add("delete-btn");
+  editButton.classList.add("edit-btn");
 
   newTaskDiv.appendChild(taskTextSpan);
+  newTaskDiv.appendChild(editButton);
   newTaskDiv.appendChild(deleteButton);
   taskListEl.appendChild(newTaskDiv);
 
@@ -29,6 +34,7 @@ const addTodo = () => {
   // console.log(inputvalue);
 };
 
+// delete feature
 taskListEl.addEventListener("click", (event) => {
   if (event.target.classList.contains("delete-btn")) {
     const taskRemove = event.target.parentElement;
